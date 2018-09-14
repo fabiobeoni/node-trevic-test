ee3db4be0505c24c745e541932d40c8728864a0f
fbfb79 <reg@beoni.net>
2018-09-14 10:44:01 +0200

    .

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;;
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
